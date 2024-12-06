309c2b815f9753491ea239286da8030b3f78d1d3
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2024-12-06 13:32:24 +0000

    wowza

---
+++ script.js
@@ -0,0 +1,4 @@
+let remindSubmit = document.querySelector('.remind-submit');
+const leftButton = document.querySelector('.leftbutton')
+const rightButton = document.querySelector('.rightbutton')
+let array
