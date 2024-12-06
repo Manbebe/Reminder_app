98467fcb531017f771bc40fa5686bed9c9f978b9
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2024-12-06 17:22:58 +0000

    java

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let remindSubmit = document.querySelector('.remind-submit');
-const leftButton = document.querySelector('.leftbutton')
-const rightButton = document.querySelector('.rightbutton')
-let array
+const leftButton = document.querySelector('.leftbutton');
+const rightButton = document.querySelector('.rightbutton');
+const submitButton = document.querySelector('.submit');
+let score = document.querySelector('.score')
+const reminderInput = document.querySelector('.reminder-input')
+const title = document.querySelector('.title')
+let reminder = document.querySelector('.reminder')
+let reminderText = document.querySelector('.reminder-input').value;
+let reminders = ['walk dog', 'do hw', 'go to tennis']
+
+document.querySelector('.submit').addEventListener('click', function() {
+    // Your code here
+});
+
+let newReminder = document.createElement('div');
+
+newReminder.className = 'reminder';
+
+newReminder.innerHTML = `<p>${reminderText}</p>`;
+
+document.querySelector('.textbox').appendChild(newReminder);
